dca3e3c99190d09d39f77fcf5b50838859bfb1e1
Cristiano Maia <criscmaia@gmail.com>
2014-12-08 20:54:38 +0000

    Link on top after the registration is completed

--- register_success.php
+++ register_success.php
@@ -9,7 +9,12 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 <body>
-    <h1>Registration successful!</h1>
+    <p>
+        [ <a href="index.php">home page</a> |
+             <a href="login.php">login</a>
+        ]
+    </p>
+    <h4>Registration successful!</h4>
     <p>You can now go back to the <a href="login.php">login page</a> and log in</p>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
